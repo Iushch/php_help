a4431aab79aee237095866c8c2b222caad493ae0
Александр Ющенко <tel.79162163939@gmail.com>
2023-10-07 16:55:16 +0300

    added: type of values in php

--- Help.php
+++ Help.php
@@ -1,22 +1,4 @@
-// комментарий
-/*    */ комментарий
-\" \" или {}- экранирование для вывода
 
-int 
-float double
-string 
-boolen 
-NULL 
-array
-object
-resource
-Callable
-
-$a = (int) '6';
-memory_get_usage(); 
-print_r(); - вывод массива
-var_dump(); - вывод множества данных
-var_export(); - выводит так, что можно скопировать 
 
 isset() - позволяет проверить существование переменной 
 gettype() - определяет тип переменной
